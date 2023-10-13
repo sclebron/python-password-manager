566f633da5fbe0ac0f6bd0bedc4ef40b28098ea5
SLeBron <sophieclebron@gmail.com>
2023-10-13 10:56:36 -0600

    added inputs and while loop

---
+++ password_manager.py
@@ -0,0 +1,14 @@
+pwd = input("what is the master password?")
+
+while True: 
+mode = input("Would you like to add a new password or view existing ones (view, add), press q to quit?").lower()
+if mode =="q":
+    break
+
+if mode == "view": 
+    pass
+elif mode == "add": 
+    pass
+else: 
+    print("Invalid mode.")
+    continue
