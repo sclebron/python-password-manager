a1e252bbae68d6710fb3ce98668829cfadba27af
SLeBron <sophieclebron@gmail.com>
2023-11-13 09:42:01 -0700

    added view function

--- password_manager.py
+++ password_manager.py
@@ -1,14 +1,16 @@
 master_pwd = input("what is the master password?")
 
 def view():
-    pass
+    with open('passwords.txt', 'r') as f:
+        for line in r.readlines():
+            print(line.rstrip())
 
 def add(): 
     name = input('Account Name: ')
     pwd = input('Password: ')
 
     with open('passwords.txt', 'a') as f:
-        f.write(name + "|" + pwd)
+        f.write(name + "|" + pwd + "\n")
 
 while True: 
 mode = input("Would you like to add a new password or view existing ones (view, add), press q to quit?").lower()
